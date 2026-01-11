090d5efe35a83aa048e9aae922dd78372c453a0e
Vyftteam <123649969+vylte-finuka@users.noreply.github.com>
2026-01-12 02:45:49 +0400

    Renaming function find_valid_jumpdest to is_valid_jumpdest.

--- crates/vuc-reader/src/uvm/runtime/src/interpreter.rs
+++ crates/vuc-reader/src/uvm/runtime/src/interpreter.rs
@@ -269,7 +269,7 @@ fn extract_function_selector_from_name(function_name: &str) -> Option<u32> {
 
 /// Helper universel : trouve le JUMPDEST à la destination demandée,
 /// ou le JUMPDEST immédiatement suivant si la destination n'est pas un label.
-fn find_valid_jumpdest(dest: usize, prog: &[u8], valid_jumpdests: &HashSet<usize>) -> bool {
+fn is_valid_jumpdest(dest: usize, prog: &[u8], valid_jumpdests: &HashSet<usize>) -> bool {
     dest < prog.len() && prog[dest] == 0x5b && valid_jumpdests.contains(&dest)
 }
 
