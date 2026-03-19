86144d49e10a298bffb34e74da97e03e32d37ddf
Vyftteam <123649969+vylte-finuka@users.noreply.github.com>
2026-03-20 00:15:47 +0400

    Updating interpreter.rs.

--- crates/vuc-reader/src/uvm/runtime/src/interpreter.rs
+++ crates/vuc-reader/src/uvm/runtime/src/interpreter.rs
@@ -8,6 +8,7 @@ use crate::lib::*;
 use crate::stack::StackUsage;
 use core::ops::Range;
 use hashbrown::HashSet;
+use serde_json::json;
 use sha3::{Digest, Keccak256};
 use std::sync::atomic::{AtomicU64, Ordering};
 use std::sync::LazyLock;
