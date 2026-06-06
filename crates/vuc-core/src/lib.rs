1aac7c2411ade64379b6f4b259969e0b52a6ce83
Vyftteam <123649969+vylte-finuka@users.noreply.github.com>
2026-06-07 02:36:18 +0400

    Fixing Lunee kernel's building.

--- crates/vuc-core/src/lib.rs
+++ crates/vuc-core/src/lib.rs
@@ -1,3 +1,16 @@
-pub mod service;
+#![cfg_attr(feature = "kernel", no_std)]
+#![cfg_attr(feature = "kernel", no_main)]
+#![cfg_attr(feature = "kernel", feature(alloc_error_handler))]
+
+#[cfg(all(feature = "std", feature = "kernel"))]
+compile_error!("features 'std' et 'kernel' sont mutuellement exclusives");
+
+extern crate alloc;
+
 pub use vuc_common::async_once_cell::AsyncOnceCell;
+
+#[cfg(feature = "std")]
+pub mod service;
+
+#[cfg(feature = "kernel")]
 pub mod lunee;
