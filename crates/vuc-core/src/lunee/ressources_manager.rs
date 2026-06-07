39fed75db785c52adfe95099d29a36f70e0b539e
Vyftteam <123649969+vylte-finuka@users.noreply.github.com>
2026-06-07 12:33:50 +0400

    Refactoring resources Lunee's management.

--- crates/vuc-core/src/lunee/ressources_manager.rs
+++ crates/vuc-core/src/lunee/ressources_manager.rs
@@ -1,13 +1,10 @@
 //___  Vyft Ltd __  (c) 2026  ___  All Rights Reserved  ___
 // ___ Kernel core named Lunee ___
-// This file is part of the Slura Lunee's kernel project, licensed under the MIT License.
-// See LICENSE file in the project root for full license information.
 // crates\vuc-core\src\lunee\ressources_manager.rs
 
 //! Gestionnaire de ressources du kernel Lunee.
-//! Il regroupe les vues (ROM, RAM, GPU, …) ainsi que le **HAL**.
 
-#![no_std]
+// ❌ Supprimé : #![no_std] — géré par lib.rs
 
 use crate::lunee::hal_manager::HalManager;
 use uefi::prelude::*;
