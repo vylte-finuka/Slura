5fdecb21374ff03c286a33a275416e26acac6656
Vyftteam <123649969+vylte-finuka@users.noreply.github.com>
2026-06-07 05:14:36 +0400

    Fixing for RPC node usage in desktop system.

--- crates/vuc-platform/src/engine_platform.rs
+++ crates/vuc-platform/src/engine_platform.rs
@@ -1,6 +1,5 @@
 use ethers::types::U256;
 use ethers::utils::keccak256;
-#[cfg(feature = "std")]
 use jsonrpsee::Methods;
 use jsonrpsee_server::Server;
 use tokio::sync::{Mutex, mpsc, broadcast}; // Ajoute broadcast
@@ -51,7 +50,6 @@ use vuc_storage::storing_access::RocksDBManagerImpl;
 use vuc_storage::storing_access::RocksDBManager;
 use reth_trie::{root::state_root, TrieAccount};
 use jsonrpsee_server::{RpcModule, HttpBody};
-#[cfg(feature = "std")]
 use tower_http::cors::{Any, CorsLayer};
 use vuc_tx::slurachain_vm::SlurachainVm;
 use uvm_runtime::lib::BTreeMap;
